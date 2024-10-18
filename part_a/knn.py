ce03035286fd486d78e4534b83296d547f10e9f4
TranVietTung29 <tranviettung292003@gmail.com>
2024-10-18 23:59:02 +0700

    Add files via upload

--- part_a/knn.py
+++ part_a/knn.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__), '..')))
 from sklearn.impute import KNNImputer
 from utils import *
 import matplotlib.pyplot as plt
